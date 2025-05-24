52a58c2340d0987014db259a0915fc5efcd0745f
caasiremos <aremoisaac@gmail.com>
2025-05-24 11:07:45 +0300

    removes dd

--- app/Http/Repositories/WalletRepository.php
+++ app/Http/Repositories/WalletRepository.php
@@ -253,7 +253,7 @@ class WalletRepository
                 $walletTransaction->save();
 
                 $wallet = Wallet::where('customer_id', $walletTransaction->customer_id)->first();
-                dd($request->amount);
+            
                 if ($wallet) {
                     $wallet->balance += $request->amount;
                     $wallet->save();
