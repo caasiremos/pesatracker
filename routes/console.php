5de3e86b87da995202b5de7534a9ca6cc5922655
caasiremos <aremoisaac@gmail.com>
2025-09-24 13:38:56 +0300

    adds scheduled commands in the right way

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:check-relworx-transaction-status')
+    ->everyTwoSeconds()   
+    ->runInBackground();  
